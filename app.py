d5da3bedce4b5d0871e3a3de923dfc41fc08085a
Tabook22 <nmtabook@gmail.com>
2023-10-17 06:30:10 +0400

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<p> In The Name of Allah"
+    return render_template('index.html')
+
+
+@app.route("/nsttopics")
+def nsttopics():
+    return render_template("nsttopics.html")
+
+
+@app.route("/assignment")
+def assignment():
+    return render_template("assignment.html")
+
+
+@app.route("/ussersac")
+def usersac():
+    return render_template("usersac.html")
+
+@app.route("/surveysc")
+def surveysc():
+    return render_template("surveysc.html")
+
+@app.route("/chatbotsc")
+def chatbotsc():
+    return render_template("chatbotsc.html")
+
+@app.route('/aiedu')
+def aiedu():
+    return render_template("aiedu.html")
+
+@app.route("/nisot")
+def nisot():
+    return render_template("nisot.html")
+
+@app.route("/ainusc")
+def ainusc():
+    return render_template("ainusc.html")
+
 
 
 if __name__=="__main__":
